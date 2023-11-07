48e789ce62e145fa10a54946dd752fd1d9b6cb42
BaranovSev <aldebaran94sev@gmail.com>
2023-11-07 16:09:16 +0300

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Stepan Baranov on 07.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
